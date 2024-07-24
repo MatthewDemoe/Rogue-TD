537ee03fdfa8ce7eb8ba3c0f87c3604b7980474a
MatthewDemoe <mattdemoe@gmail.com>
2024-07-24 11:31:54 -0400

    Create generic spawnable enemies

---
+++ Assets/Scripts/StringLookups/AddressableLabels.cs
@@ -0,0 +1,11 @@
+
+namespace Lookups
+{
+    public static class AddressableLabels
+    {
+        public static string enemy => "enemy";
+        public static string easy => "easy";
+        public static string medium => "medium";
+        public static string hard => "hard";
+    }
+}
